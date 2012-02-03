ac3bd44ac1f70f696ae3baa417f0d8a86a900059
Phil Monroe <phil@philmonroe.com>
2012-02-02 17:10:23 -0800

    modifying the max_flops kernel to be beefier

--- part_b/1_num_flops/max_flops_kernel.cu
+++ part_b/1_num_flops/max_flops_kernel.cu
@@ -1,7 +1,28 @@
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 
-#define NUM_FLOPS_PER_KERNEL 100
+#define N_LOOPS				100
+#define N_FLOPS_PER_BLOCK	32
+#define N_FLOPS_PER_LOOP	N_FLOPS_PER_BLOCK * 32
+#define N_FLOPS_PER_KERNEL	N_FLOPS_PER_LOOP * N_LOOPS + 32
+
+#define FLOPS_BLOCK \
+			reg0  = reg1  * reg2  + reg3;  \
+			reg1  = reg2  * reg3  + reg4;  \
+			reg2  = reg3  * reg4  + reg5;  \
+			reg3  = reg4  * reg5  + reg6;  \
+			reg4  = reg5  * reg6  + reg7;  \
+			reg5  = reg6  * reg7  + reg8;  \
+			reg6  = reg7  * reg8  + reg9;  \
+			reg7  = reg8  * reg9  + reg10; \
+			reg8  = reg9  * reg10 + reg11; \
+			reg9  = reg10 * reg11 + reg12; \
+			reg10 = reg11 * reg12 + reg13; \
+			reg11 = reg12 * reg13 + reg14; \
+			reg12 = reg13 * reg14 + reg15; \
+			reg13 = reg14 * reg15 + reg0;  \
+			reg14 = reg15 * reg0  + reg1;  \
+			reg15 = reg0  * reg1  + reg2;
 
 //-----------------------------------------------------------------------------
 // Simple test kernel template for flops test
@@ -9,66 +30,56 @@
 // 	@param n_threads	- Total number of threads per block
 //-----------------------------------------------------------------------------
 __global__ 
-void max_flops_kernel(float* d_counters, int num_threads) {
+void max_flops_kernel(float* d_counters) {
     // Increment the counter
-	int i = blockIdx.x * num_threads + threadIdx.x;
-	float temp = 1.0f;
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
+	// Declare a bunch or registers and init to 0.0f
+	float reg0, reg1, reg2,  reg3,  reg4,  reg5,  reg6,  reg7, \
+			reg8, reg9, reg10, reg11, reg12, reg13, reg14, reg15;
 	
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
+	// 1 FLOP per assignment = 16 FLOPs total
+	reg0  = reg1  = reg2  = reg3  = 0.125f * threadIdx.x;
+	reg4  = reg5  = reg6  = reg7  = 0.125f * threadIdx.y;
+	reg8  = reg9  = reg10 = reg11 = 0.5f 	* threadIdx.x;
+	reg12 = reg13 = reg14 = reg15 = 0.5f 	* threadIdx.y;
 	
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
-	temp = temp + .05f * temp;
+	for(int i = 0; i < N_LOOPS; ++i){
+		FLOPS_BLOCK // 1
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK // 8
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK // 16
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK // 24
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK
+		FLOPS_BLOCK  // 32
+	}
 
-	
-	d_counters[i] = temp;
+	// 16 More flops.
+	d_counters[i] = reg0 + reg1 + reg2  + reg3  + reg4  + reg5  + reg6  + reg7  + reg8 \
+								+ reg9 + reg10 + reg11 + reg12 + reg13 + reg14 + reg15 + 8.0f;
 }
