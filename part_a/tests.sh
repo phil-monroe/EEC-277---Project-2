74e517abe973c56663fe1dcdbcdf76814e869c43
Phil Monroe <phil@philmonroe.com>
2012-02-06 13:22:18 -0800

    makeing the tests use area tests

--- part_a/tests.sh
+++ part_a/tests.sh
@@ -1,32 +1,32 @@
-echo "AREA TEST ---------------------------------------------------------------"
-./wesBench-instructional -doareatest
+# echo "AREA TEST ---------------------------------------------------------------"
+# ./wesBench-instructional -doareatest
 
 echo "LIGHT TEST --------------------------------------------------------------"
-./wesBench-instructional -light
+./wesBench-instructional -light -doareatest
 
 echo "TEXTURE TEST ------------------------------------------------------------"
-./wesBench-instructional -tx 128
+./wesBench-instructional -tx 128 -doareatest
 
 echo "TEXTURE AND LIGHT TEST --------------------------------------------------"
-./wesBench-instructional -tx 128 -light
+./wesBench-instructional -tx 128 -light -doareatest
 
 echo "DISJOINT TEST -----------------------------------------------------------"
-./wesBench-instructional -tt 0
+./wesBench-instructional -tt 0 -doareatest
 
 echo "TSTRIP TEST -------------------------------------------------------------"
-./wesBench-instructional -tt 1
+./wesBench-instructional -tt 1 -doareatest
 
 echo "INDEXED DISJOINT TEST ---------------------------------------------------"
-./wesBench-instructional -tt 2
+./wesBench-instructional -tt 2 -doareatest
 
 echo "INDEXED TSTRIP TEST -----------------------------------------------------"
-./wesBench-instructional -tt 3
+./wesBench-instructional -tt 3 -doareatest
 
-echo "VERTEX TEST -------------------------------------------------------------"
-./wesBench-instructional -dovbtest
-
-echo "TEXTURE TEST ------------------------------------------------------------"
-./wesBench-instructional -dotxtest
+# echo "VERTEX TEST -------------------------------------------------------------"
+# ./wesBench-instructional -dovbtest
+# 
+# echo "TEXTURE TEST ------------------------------------------------------------"
+# ./wesBench-instructional -dotxtest
 
 
 
