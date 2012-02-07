bf3428d26e42dc6db049887a82132e50a4b54fc7
Phil Monroe <phil@philmonroe.com>
2012-02-07 10:09:44 -0800

    cleaning up branch perf

--- part_b/5_branch_perf/main.cu
+++ part_b/5_branch_perf/main.cu
@@ -1,6 +1,12 @@
 #ifndef _MAX_FLOPS_
 #define _MAX_FLOPS_
 
+// Define Defaults ------------------------------------------------------------
+#define NUM_BLOCKS 				128
+#define NUM_THREADS_PER_BLOCK 128	//	Taken from CUDA Occupancy Calc to maximize occupancy
+#define NUM_LOOPS		 			32
+#define ARRAY_SIZE				(NUM_BLOCKS * NUM_THREADS_PER_BLOCK)
+
 // includes -------------------------------------------------------------------
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,11 +17,8 @@
 #include <cuda_runtime_api.h>
 
 #include "kernel.cu"	// Kernel to Maximize FLOPS
+#include "common/helpers.c"
 
-// Defines --------------------------------------------------------------------
-#define NUM_BLOCKS 				128
-#define NUM_THREADS_PER_BLOCK 128	//	Taken from CUDA Occupancy Calc to maximize occupancy
-#define NUM_ITERATIONS 			32
 
 // Forward Declarations --------------------------------------------------------
 float runTest( int num_branches);
@@ -23,14 +26,14 @@ void init_counters(float** h_counters, float** d_counters, unsigned int num_coun
 
 // Main -----------------------------------------------------------------------
 int main( int argc, char** argv) {
-	printf("Testing Number of Branched vs. Performance");
-	printf("Number of Threads/Blocks: %4d\n", NUM_THREADS_PER_BLOCK);
-	printf("\n");
+	printf("Testing Number of Branched vs. Performance\n");
+	printf("Written by Phil Monroe and Kramer Straube\n");
+	printf("Number of Threads/Blocks: %4d\n\n", NUM_THREADS_PER_BLOCK);
 	
 	FILE *file; 
 	file = fopen("out.csv","a+");
 	
-	for(int iter = 0; iter < NUM_ITERATIONS; ++iter){
+	for(int iter = 0; iter < NUM_LOOPS; ++iter){
 		printf("Iteration %d\n", iter);
 		float perf = runTest(iter+1); 
 		fprintf(file, "%d, %d, %f\n", iter, iter+1, perf);
@@ -45,56 +48,29 @@ int main( int argc, char** argv) {
 //
 float runTest( int num_branches) {
 	printf("Number of branches: %d \n", num_branches);
-	// Hardware Dependent - NV GeForce 9500 GT
 	unsigned int threads = NUM_BLOCKS * NUM_THREADS_PER_BLOCK;	
 
 	// Initialize counters on host and device to 0.0f
 	printf("Init counters\n");
 	float *h_counters, *d_counters;
-	init_counters(&h_counters, &d_counters, threads);
+	initArray(&h_counters, &d_counters, threads);
 
 
 	// Create and Start Timer
-	printf("Starting Test\n");
 	cudaEvent_t start, stop;
-	float time;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord( start, 0 );
+	startTest(start, stop);
 
 	// Run the test
 	branch_perf_kernel<<< NUM_BLOCKS, NUM_THREADS_PER_BLOCK>>>(d_counters, num_branches);	
 	
-	// Record end time
-	cudaEventRecord( stop, 0 );
-	cudaEventSynchronize( stop );
-	cudaEventElapsedTime( &time, start, stop );
-	cudaEventDestroy( start );
-	cudaEventDestroy( stop );
-	float time_s = time/1000.0f;
-	printf("Finished Test in %f s\n", time_s);
-
-	// Check for errors
-	cudaError_t error = cudaGetLastError();
-	if(error != cudaSuccess)
-		printf("Error: %s\n", cudaGetErrorString( error ));
-		
-	// Check array
-	cudaMemcpy(h_counters, d_counters, threads * sizeof(float), cudaMemcpyDeviceToHost);
-	// 
-	// for(int i = 0; i < threads; ++i){
-	// 	printf("Thread %d: %f\n", i, h_counters[i]);
-	// }
+	// Get the time elapsed
+	float time_s = finishTest(start, stop);
 
 	// Calculate GFLOPS
 	unsigned long long total_flops = N_FLOPS_PER_THREAD * NUM_BLOCKS * NUM_THREADS_PER_BLOCK;
 	printf("Total FLOPs: %lld\n", total_flops);
 	float gflops = total_flops/(time_s*1000000000.0f);
 	printf("GFLOPS: %.3f\n", gflops);
-	
-	printf("\n");
-	
-
 
 	// Cleanup
 	free(h_counters);
