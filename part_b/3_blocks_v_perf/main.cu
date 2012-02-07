a7dae99046e3e99e84f8c6b5a7f724f4786f85e4
Phil Monroe <phil@philmonroe.com>
2012-02-07 09:24:07 -0800

    cleaning up blocks_v_perf

--- part_b/3_blocks_v_perf/main.cu
+++ part_b/3_blocks_v_perf/main.cu
@@ -1,6 +1,14 @@
 #ifndef _BLOCKS_V_PERF_
 #define _BLOCKS_V_PERF_
 
+// Defines --------------------------------------------------------------------
+// Hardware Dependent - NV GeForce 9500 GT
+
+#define NUM_BLOCKS				0		// Gets iterated over
+#define ARRAY_SIZE				0		//	Gets changed with every iteration
+#define NUM_THREADS_PER_BLOCK 384	//	Taken from CUDA Occupancy Calc to maximize occupancy
+#define NUM_LOOPS					100
+
 // includes -------------------------------------------------------------------
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,27 +19,23 @@
 #include <cuda_runtime_api.h>
 
 #include "kernel.cu"	// Kernel to Maximize FLOPS
+#include "common/helpers.c"
 
-// Defines --------------------------------------------------------------------
-// Hardware Dependent - NV GeForce 9500 GT
 
-#define NUM_THREADS_PER_BLOCK 384	//	Taken from CUDA Occupancy Calc to maximize occupancy
-#define NUM_ITERATIONS			100
 
 // Forward Declarations --------------------------------------------------------
-void init_counters(float** h_counters, float** d_counters, unsigned int num_counters);
 float runTest(int num_blocks);
 
 // Main -----------------------------------------------------------------------
 int main( int argc, char** argv) {
-	printf("Testing Number of Blocks vs. Performance");
-	printf("Number of Threads/Blocks: %4d\n", NUM_THREADS_PER_BLOCK);
-	printf("\n");
+	printf("Testing Number of Blocks vs. Performance\n");
+	printf("Written by Phil Monroe and Kramer Straube\n\n");
+	printf("Number of Threads/Blocks: %4d\n\n", NUM_THREADS_PER_BLOCK);
 	
 	FILE *file; 
 	file = fopen("out.csv","a+");
 	
-	for(int iter = 0; iter < NUM_ITERATIONS; ++iter){
+	for(int iter = 0; iter < NUM_LOOPS; ++iter){
 		printf("Iteration %d\n", iter);
 		float perf = runTest(iter+1); 
 		fprintf(file, "%d, %d, %f\n", iter, iter+1, perf);
@@ -49,44 +53,20 @@ float runTest( int num_blocks) {
 	
 	printf("Testing %4d Blocks\n", num_blocks);
 	int num_threads = num_blocks * NUM_THREADS_PER_BLOCK;
-	
 
 	// Initialize counters on host and device to 0.0f
 	float *h_counters, *d_counters;
-	init_counters(&h_counters, &d_counters, num_threads);
-
+	initArray(&h_counters, &d_counters, num_threads);
 
 	// Create and Start Timer
 	cudaEvent_t start, stop;
-	float time;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord( start, 0 );
+	startTest(start, stop);
 
 	// Run the test
 	max_flops_kernel<<< num_blocks, NUM_THREADS_PER_BLOCK>>>(d_counters);
 	
-	
-	// Record end time
-	cudaEventRecord( stop, 0 );
-	cudaEventSynchronize( stop );
-	cudaEventElapsedTime( &time, start, stop );
-	cudaEventDestroy( start );
-	cudaEventDestroy( stop );
-	float time_s = time/1000.0f;
-	printf("Finished Test in %f s\n", time_s);
-
-	// Check for errors
-	cudaError_t error = cudaGetLastError();
-	if(error != cudaSuccess)
-		printf("Error: %s\n", cudaGetErrorString( error ));
-		
-	// Check array
-	//cudaMemcpy(h_counters, d_counters, threads * sizeof(float), cudaMemcpyDeviceToHost);
-	
-	// for(int i = 0; i < threads; ++i){
-	// 	printf("Thread %d: %f\n", i, h_counters[i]);
-	// }
+	// Get the time elapsed
+	float time_s = finishTest(start, stop);
 
 	// Calculate Performance
 	unsigned long long total_flops = (long long)N_FLOPS_PER_KERNEL * (long long)num_blocks * (long long)NUM_THREADS_PER_BLOCK;
@@ -103,20 +83,6 @@ float runTest( int num_blocks) {
 	cudaFree(d_counters);
 	
 	return gflops;
-}
-
-// init_counters --------------------------------------------------------------
-//		Initializes an array of floats that will be used to count FLOPS.
-//
-void init_counters(float** h_counters, float** d_counters, unsigned int num_counters){
-	*h_counters = (float*) malloc( num_counters * sizeof(float));   // Allocate counters on host
-	cudaMalloc((void **) d_counters, num_counters*sizeof(float));   // Allocate counters on device
-
-	// Initialize host counters to 0.0 ...
-	for( unsigned int i = 0; i < num_counters; ++i)
-		(*h_counters)[i] = 0.0f;
-	// ... and copy to device
-	cudaMemcpy(*d_counters, *h_counters, num_counters * sizeof(float), cudaMemcpyHostToDevice);
 }
 
 
